da5db22325724ea44fcc9c7b8b44cb0acb9fc6bf
PaulaJansen <paulamarijkejansen@gmail.com>
2025-10-23 00:03:00 +0200

    Initial commit: frontend + backend structure

---
+++ backend/Poc-backend/src/test/java/nl/novi/endassignment/pocbackend/PocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.endassignment.pocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
